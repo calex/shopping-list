4e3eb238bf7ce3263e31e84fde755e0aa4def45f
Chris.Alexander <chris.alexander@mac.com>
2020-04-29 19:49:54 -0700

    created shopping list by adding jQuery, writing code to make interactive.

---
+++ index.js
@@ -0,0 +1,33 @@
+function shoppingListControls() {
+    $('#js-shopping-list-form').submit(event => {
+        event.preventDefault();
+
+        const stringToAdd = $(event.currentTarget).find('#shopping-list-entry').val();
+
+        $('.shopping-list').append(`
+            <li>
+                <span class="shopping-item">${stringToAdd}</span>
+                <div class="shopping-item-controls">
+                <button class="shopping-item-toggle">
+                    <span class="button-label">check</span>
+                </button>
+                <button class="shopping-item-delete">
+                    <span class="button-label">delete</span>
+                </button>
+                </div>
+            </li>`
+        );
+    }); 
+
+    $('.shopping-list').on('click', '.shopping-item-delete', event => {
+        event.currentTarget.closest('li').remove();
+    }); 
+
+    $('.shopping-list').on('click', '.shopping-item-toggle', event => {
+        const parentListItem = event.currentTarget.closest('li');
+        
+        $(parentListItem).find('.shopping-item').toggleClass('shopping-item__checked');
+    }); 
+}
+
+$(shoppingListControls);
